4b4914519406e90cdc5ad3e42c99556395fe2894
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2024-09-21 20:49:59 +0600

    transfer section done

--- home.js
+++ home.js
@@ -1,10 +1,15 @@
 const addMoneyBtn = document.getElementById('add-money-btn')
 addMoneyBtn.addEventListener('click', function(event){
     event.preventDefault()
-    const loginButton = document.getElementById('login-btn')
+    const addSection = document.getElementById('add-section')
+    addSection.classList.remove('hidden')
+    const loginButton = document.getElementById('cash-in-btn')
     loginButton.removeAttribute('disabled')
+    const outSection = document.getElementById('out-section')
+    outSection.classList.add('hidden')
 })
-document.getElementById('login-btn').addEventListener('click', function(event){
+
+document.getElementById('cash-in-btn').addEventListener('click', function(event){
     event.preventDefault()
     const availableMoney = parseFloat(document.getElementById('available-money').innerText)
     const enterAmount = parseFloat(document.getElementById('enter-amount').value)
@@ -14,20 +19,43 @@ document.getElementById('login-btn').addEventListener('click', function(event){
         document.getElementById('available-money').innerText = addMoney
         document.getElementById('enter-amount').value = ''
         document.getElementById('input-password').value = ''
-        const loginButton = document.getElementById('login-btn')
+        const loginButton = document.getElementById('cash-in-btn')
         loginButton.setAttribute('disabled', true)
     }else{
         alert('Please provide Right Information')
     }
     
 })
+
 const logOutButton = document.getElementById('log-out-button')
 logOutButton.addEventListener('click', function(event){
     event.preventDefault()
     window.location.href = 'index.html'
 })
-
-
+document.getElementById('cash-out-money-btn').addEventListener('click', function(){
+    const outSection = document.getElementById('out-section')
+    outSection.classList.remove("hidden")
+    const cashOutBtn = document.getElementById('cash-out-btn')
+    cashOutBtn.removeAttribute('disabled')
+    const cashOutSection = document.getElementById('add-section')
+    cashOutSection.classList.add('hidden')
+})
+document.getElementById('cash-out-btn').addEventListener('click', function(event){
+    event.preventDefault()
+    const availableMoney = parseFloat(document.getElementById('available-money').innerText)
+    const enterAmount = parseFloat(document.getElementById('cash-out-amount').value)
+    const InputPassword = document.getElementById('cas-out-password').value
+    if(InputPassword === '1234' && enterAmount > 0 && enterAmount < availableMoney){
+        let reduceMoney = availableMoney - enterAmount
+        document.getElementById('available-money').innerText = reduceMoney
+        document.getElementById('cash-out-amount').value = ''
+        document.getElementById('cas-out-password').value = ''
+        const loginButton = document.getElementById('cash-out-btn')
+        loginButton.setAttribute('disabled', true)
+    }else{
+        alert('Please provide Right Information')
+    }
+})
 
 
 

781a826fcf8247c32847e03cfa08350863fabd77
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2024-09-28 11:44:49 +0600

    short some code

---
+++ utilities.js
@@ -0,0 +1,23 @@
+function showSectionById(id){
+    getDocumentElementIdOnly('add-section').classList.add('hidden')
+    getDocumentElementIdOnly('out-section').classList.add('hidden')
+    getDocumentElementIdOnly('transfer-section').classList.add('hidden')
+    getDocumentElementIdOnly('bonus-section').classList.add('hidden')
+    getDocumentElementIdOnly('pay-section').classList.add('hidden')
+    getDocumentElementIdOnly('transaction-section').classList.add('hidden')
+    document.getElementById(id).classList.remove('hidden')
+}
+
+function hideDisabledOption(id){
+    getDocumentElementIdOnly(id).removeAttribute('disabled')
+}
+
+function selectButtonDisabled(id){
+    const buttonDtn = document.getElementsByClassName(id)
+    buttonDtn[0].setAttribute('disabled', true)
+    buttonDtn[1].setAttribute('disabled', true)
+    buttonDtn[2].setAttribute('disabled', true)
+    buttonDtn[3].setAttribute('disabled', true)
+    buttonDtn[4].setAttribute('disabled', true)
+    buttonDtn[5].setAttribute('disabled', true)
+}
